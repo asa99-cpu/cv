40b0e1d4dcbabeb69c54de2c0d0ac8e1f4449f7c
asa99-cpu <58121161+asa99-cpu@users.noreply.github.com>
2025-02-08 12:43:00 +0300

    Update introduction.py

--- sections/introduction.py
+++ sections/introduction.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 def show():
-    st.header("Introduction")
-    st.write("""
-        I am a dedicated laboratory technician with experience in eco-management and a background in Chemistry. 
-        I am skilled in using various software tools such as Microsoft Office and video editing. 
-        I am passionate about contributing my skills to the field and constantly improving my expertise.
-    """)
+    st.header("Personal Details")
+    st.write("**Name:** Derin Najmadin Mahamd")
+    st.write("**Phone number:** 0750 710 40 32")
+    st.write("**Email:** deman.najmadin@gmail.com")
+    st.write("**Date of Birth:** September 9, 1995")
+    st.write("**Gender:** Female")

d6aec7ee3454f25cbc778fa98689eafd9a2f26c1
asa99-cpu <58121161+asa99-cpu@users.noreply.github.com>
2025-02-08 12:35:47 +0300

    Update skills.py

--- sections/skills.py
+++ sections/skills.py
@@ -1,1 +1,16 @@
+import streamlit as st
+
+def show():
+    st.header("Skills")
+    st.write("**Technical Skills**")
+    st.write("- Laboratory Technician")
+    st.write("- Eco-Management")
+    st.write("- Microsoft Office")
+    st.write("- Video Editing")
+
+    st.write("**Languages**")
+    st.write("- Kurdish")
+    st.write("- Arabic")
+    st.write("- English")
+    st.write("- Persian")
 

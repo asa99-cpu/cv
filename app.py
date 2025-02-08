c737a4532c05b4a73bfe0b4b8e3b9559ca5293b1
asa99-cpu <58121161+asa99-cpu@users.noreply.github.com>
2025-02-08 12:42:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,18 @@
 import streamlit as st
-from sections import introduction, education, experience, skills, contact
-
-st.markdown(f'<style>{open("static/styles.css").read()}</style>', unsafe_allow_html=True)
+from sections import introduction, skills, education, languages, internships, contact
 
 def main():
-    st.title("Derin Najmadin Mahamd - CV")
+    st.title("Derin Najmadin Mahamd's CV")
 
-    # Display Profile Image
+    # Display profile image
     st.image("assets/profile_image.jpg", width=150)
-    st.subheader("Personal Information")
-    st.write("**Name:** Derin Najmadin Mahamd")
-    st.write("**Email:** deman.najmadin¢0@gmail.com")
-    st.write("**Phone:** 0750 710 40 32")
-    st.write("**Date of Birth:** September 9, 1995")
-    st.write("**Gender:** Female")
-    st.write("**Nationality:** Kurdish")
-    st.write("**Languages:** Kurdish, Arabic, English, Persian")
 
     # Call different sections of the CV
     introduction.show()
-    education.show()
-    experience.show()
     skills.show()
+    education.show()
+    languages.show()
+    internships.show()
     contact.show()
 
 if __name__ == "__main__":

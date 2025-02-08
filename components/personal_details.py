5c201b6fb4a1a49421234db0736ff8a91adf7986
asa99-cpu <58121161+asa99-cpu@users.noreply.github.com>
2025-02-08 13:27:58 +0300

    Delete components directory

--- components/personal_details.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def show():
-    st.header("Contact")
-    st.write("You can contact me via email at: [deman.najmadin¢0@gmail.com](mailto:deman.najmadin¢0@gmail.com)")
-    st.write("Phone: 0750 710 40 32")
-

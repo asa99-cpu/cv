f6d10f73bb4444bfa6f9bdc48b4274b5cdb411f9
asa99-cpu <58121161+asa99-cpu@users.noreply.github.com>
2025-02-08 12:35:20 +0300

    Update introduction.py

--- sections/introduction.py
+++ sections/introduction.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+def show():
+    st.header("Introduction")
+    st.write("""
+        I am a dedicated laboratory technician with experience in eco-management and a background in Chemistry. 
+        I am skilled in using various software tools such as Microsoft Office and video editing. 
+        I am passionate about contributing my skills to the field and constantly improving my expertise.
+    """)

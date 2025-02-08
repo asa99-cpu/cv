2818692a7dbb61e1e9bc4c10c90c2c517a6435e2
asa99-cpu <58121161+asa99-cpu@users.noreply.github.com>
2025-02-08 11:27:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,7 @@
 import os
-import pdfkit
+import weasyprint
 from jinja2 import Template
 from io import BytesIO
-import subprocess
-
-# Try to find the path of wkhtmltopdf in the environment
-def find_wkhtmltopdf():
-    try:
-        # Check if wkhtmltopdf is installed in the environment
-        result = subprocess.run(['which', 'wkhtmltopdf'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        return result.stdout.decode().strip()  # Return the path
-    except Exception as e:
-        return None
-
-wkhtmltopdf_path = find_wkhtmltopdf()
-
-if not wkhtmltopdf_path:
-    raise Exception("wkhtmltopdf is not installed in the environment")
-
-# Manually set the wkhtmltopdf path for pdfkit
-PDFKIT_CONFIG = pdfkit.configuration(wkhtmltopdf=wkhtmltopdf_path)
 
 def create_pdf_cv(data):
     # Load HTML template
@@ -29,10 +11,8 @@ def create_pdf_cv(data):
     # Render the HTML content with provided data
     html_content = template.render(data)
 
-    # Convert HTML to PDF
+    # Convert HTML to PDF using WeasyPrint
     pdf_buffer = BytesIO()
-    pdfkit.from_string(html_content, "cv.pdf", configuration=PDFKIT_CONFIG)
-    with open("cv.pdf", "rb") as f:
-        pdf_buffer.write(f.read())
+    weasyprint.HTML(string=html_content).write_pdf(pdf_buffer)
     pdf_buffer.seek(0)
     return pdf_buffer

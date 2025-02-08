17316065c6e70eacef944be411ddf64e17a1ecae
asa99-cpu <58121161+asa99-cpu@users.noreply.github.com>
2025-02-08 12:35:57 +0300

    Update contact.py

--- sections/contact.py
+++ sections/contact.py
@@ -1,1 +1,7 @@
+import streamlit as st
+
+def show():
+    st.header("Contact")
+    st.write("You can contact me via email at: [deman.najmadin¢0@gmail.com](mailto:deman.najmadin¢0@gmail.com)")
+    st.write("Phone: 0750 710 40 32")
 
